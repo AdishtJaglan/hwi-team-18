05cb67a2aee991ffcbff246f29f8f771766412b7
AdishtJaglan <adisht7524@gmail.com>
2025-08-24 13:05:12 +0530

    ready endpoint to integrate with model

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
-from .views import raw_upload
+from .views import raw_upload, extract_location_and_fetch_images
 
 urlpatterns = [
     path('upload_raw/', raw_upload, name='raw-upload'),
+    path('extract_and_fetch/', extract_location_and_fetch_images, name='extract-and-fetch'),
 ]
